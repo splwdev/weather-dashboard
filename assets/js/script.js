1c70c2b58b957c738c4785f7edecaedda9c5ea09
splwebdev <shane_lynch@hotmail.com>
2023-01-25 22:29:47 +0000

    added pseudo code and global variables to script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// Declare variables for html elements
+// function / event handler for search button
+// ajax to connect to api
+// buttons for search history (max 6)
+    // saved to localstorage
+    // click on history buttons bring up their current weather / forecast
+// current weather view
+    // heading for city name
+    // heading for date
+    // img for icon
+    // paragraph for temperature
+    // paragraph for humidity
+    // paragraph for wind speed
+// forecast weather view
+    // 5 columns
+    // heading for date
+    // img for icon
+    // paragraph for temperature
+    // paragraph for humidity
+// button to clear localstorage
+
+
+var searchForm = $("#search-form");
+
+var searchInput = $("#search-input");
+
+var searchButton = $("#search-button");
+
+var searchHistory = $("#history");
+
+var currentWeather = $("#today");
+
+var forecast = $("#forecast");
