d56e1c49d935a3ac5238caeef3735f982955b698
splwebdev <shane_lynch@hotmail.com>
2023-01-28 13:28:54 +0000

    added comments in code / done some tidying up of files / add greeting to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,47 +27,49 @@
 // button to clear localstorage
 
 // TODO
-    // Local Storage + Buttons for history
-    // CSS styles
+    // upper and lower case letters for city names
+    // add checks if city name isnt found in openweathermap db
 
-
+// Global variables to be used as part of the application
 var searchForm = $("#search-form");
-
 var searchInput = $("#search-input");
-
 var searchButton = $("#search-button");
-
 var searchHistory = $("#history");
-
 var currentWeather = $("#today");
-
 var forecast = $("#forecast");
-
+// Initialising local storge
 var cityName = localStorage.getItem("cityName");
-
+// Initialising array
 var cityArr = [];
-
+// API key
 const APIKey = "5350d0f7b1fc4bfd19dd68ba95e8ac1b";
-
+// Initialsing searchCity string
 var searchCity = "";
 
+// Recalling previous history
 cityHistory();
 
+// Search button event handler
 $(searchButton).on("click", function (event) {
     event.preventDefault();
     searchCity = $(searchInput).val();
+    // Checking to see if the user has already searched for the city
     if (cityArr.includes(searchCity) === true) { 
         alert("You have previously searched for this city, please select it from your history list");
         return;
     }
+    // pushing searchCity input into the array, then adding the array data into local storage
     cityArr.push(searchCity);
     localStorage.setItem("cityName", JSON.stringify(cityArr));
+    // hiding the landing page greeting
+    $("#greeting").addClass("hide");
     renderButtons();
     getCurrentWeather();
     getForecast();
     searchInput.val("");
 });
 
+// event handler to display weather information for city button clicked on in history
 $("#history").on("click", "button", function (event) {
     event.preventDefault();
     var cityBtn = $(event.target);
@@ -76,6 +78,7 @@ $("#history").on("click", "button", function (event) {
     getForecast();
 })
 
+// event handler to clear local storage, array, and the screen
 $("#clearBtn").on("click", function () {
     clearStorage();
     $("#history").empty();
@@ -83,6 +86,7 @@ $("#clearBtn").on("click", function () {
     $(forecast).empty();
 })
 
+// function to get current weather
 function getCurrentWeather() {
     $(currentWeather).empty();
     var cwqueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + APIKey + "&units=metric";
@@ -101,6 +105,7 @@ function getCurrentWeather() {
     });
 }
 
+// function to get forecast
 function getForecast() {
     $(forecast).empty();
     var fqueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=" + APIKey + "&units=metric";
@@ -112,8 +117,9 @@ function getForecast() {
         var forecastArea = $("<div>").attr("id", "forecastArea").addClass("col-lg-12 d-flex flex-row");
         $(forecast).append(forecastTitle, forecastArea);
         for (i = 0; i < 5; i++) {
+            // this line ensures that the weather is displayed for the same time each day for the forecast
             var localDate = moment.unix(response.list[((i + 1) * 8) - 1].dt + response.city.timezone).format("DD-MM-YYYY");
-            var forecastCard = $("<div>").addClass("col-md-2 p-3 mx-2").attr("id", "forecastCard");
+            var forecastCard = $("<div>").addClass("col-md-2 p-3 mx-2 rounded").attr("id", "forecastCard");
             var forecastDate = $("<h4>").text(localDate);
             var forecastImage = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + ".png").attr("class", "justify-content-center");
             var forecastTemp = $("<p>").text("Temp: " + response.list[i].main.temp + " °C");
@@ -125,6 +131,7 @@ function getForecast() {
     });
 }
 
+// function to render the buttons for the city search history
 function renderButtons() {
     $("#history").empty();
     for (i = 0; i < cityArr.length; i++) {
@@ -132,7 +139,8 @@ function renderButtons() {
         $("#history").append(cityBtn);
     }
 }
- 
+
+// function to set the city array to the local storage data then render buttons
 function cityHistory() {
     var searchHistory = JSON.parse(localStorage.getItem("cityName"));
     if (searchHistory !== null) {
@@ -141,8 +149,10 @@ function cityHistory() {
     renderButtons();
 }
 
+// function to clear storage
 function clearStorage() {
     window.localStorage.clear();
+    cityArr = [];
 }
 
 
